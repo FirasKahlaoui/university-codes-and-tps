bb8e5e7c4aa7a1d48de698f14c900ef1c306a07b
FirasKahlaoui <kahlaouifiras2017@gmail.com>
2024-10-05 11:57:52 +0100

    Database logic

---
+++ Security/secure_cipher/database.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    is_verified = db.Column(db.Boolean, default=False)
+
+def create_db():
+    db.create_all()
