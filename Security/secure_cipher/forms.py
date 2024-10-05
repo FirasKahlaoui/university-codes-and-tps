2ff3d4038d8aa11c05a6b491de01b00f2a16e3f5
FirasKahlaoui <kahlaouifiras2017@gmail.com>
2024-10-05 11:59:16 +0100

    Forms

---
+++ Security/secure_cipher/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
---
+++ Security/secure_cipher/two_factor.py
@@ -0,0 +1,4 @@
+import random
+
+def generate_verification_code():
+    return random.randint(100000, 999999)
