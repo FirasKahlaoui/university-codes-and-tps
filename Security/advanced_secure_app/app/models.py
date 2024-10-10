fb2ce7715790a01ccf9e91b7659ea1e6e3199287
FirasKahlaoui <kahlaouifiras2017@gmail.com>
2024-10-10 21:07:41 +0100

    Define the database schema

---
+++ Security/advanced_secure_app/app/models.py
@@ -0,0 +1,18 @@
+from app import db
+from flask_login import UserMixin # type: ignore
+from datetime import datetime
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    action = db.Column(db.String(100), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
