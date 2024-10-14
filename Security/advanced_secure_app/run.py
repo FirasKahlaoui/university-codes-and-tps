9114b8d63ae6e898ddb5c626237105bc19e9cbef
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-10-14 11:45:10 +0100

    Render the styles.css

--- Security/advanced_secure_app/run.py
+++ Security/advanced_secure_app/run.py
@@ -1,14 +1,11 @@
 from app import create_app
-from flask import Flask, redirect, url_for
+from flask import redirect, url_for
 
-app = Flask(__name__, static_folder='app/static')
-app = create_app(app)
-
+app = create_app()
 
 @app.route('/')
 def index():
     return redirect(url_for('main.login'))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
