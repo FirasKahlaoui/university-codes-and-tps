186970c57cbb5e0268b8095b4ec763e69a1b3ffa
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-10-16 22:43:56 +0100

    User Model is Update

--- Security/advanced_secure_app/app/models.py
+++ Security/advanced_secure_app/app/models.py
@@ -1,15 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import db, login_manager
-from flask_login import UserMixin
+from flask_login import UserMixin, LoginManager
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id)) or Admin.query.get(int(user_id))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,6 +25,7 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
 
 class Admin(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
