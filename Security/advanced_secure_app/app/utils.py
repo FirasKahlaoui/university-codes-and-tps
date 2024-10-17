6e53dcd8b83418c787bc9bc631b75a5209a8bef7
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-10-17 12:21:47 +0100

    Add the 128 bits/192/256

--- Security/advanced_secure_app/app/utils.py
+++ Security/advanced_secure_app/app/utils.py
@@ -13,16 +13,16 @@ db_handler.setLevel(logging.INFO)
 logger = logging.getLogger('db_logger')
 logger.addHandler(db_handler)
 
-
-def encrypt_text(plaintext, key):
+def encrypt_text(plaintext, key, key_size=128):
+    key = key.ljust(key_size // 8, '\0')[:key_size // 8]  # Adjust key length
     cipher = AES.new(key.encode('utf-8'), AES.MODE_CBC)
     ct_bytes = cipher.encrypt(pad(plaintext.encode('utf-8'), AES.block_size))
     iv = base64.b64encode(cipher.iv).decode('utf-8')
     ct = base64.b64encode(ct_bytes).decode('utf-8')
     return iv + ct
 
-
-def decrypt_text(encrypted_text, key):
+def decrypt_text(encrypted_text, key, key_size=128):
+    key = key.ljust(key_size // 8, '\0')[:key_size // 8]  # Adjust key length
     iv = base64.b64decode(encrypted_text[:24])
     ct = base64.b64decode(encrypted_text[24:])
     cipher = AES.new(key.encode('utf-8'), AES.MODE_CBC, iv)
@@ -30,13 +30,9 @@ def decrypt_text(encrypted_text, key):
     return pt.decode('utf-8')
 
 # Log user actions
-
-
 def log_action(user_id, action):
-    log_entry = Log(user_id=user_id, action=action,
-                    timestamp=datetime.utcnow())
+    log_entry = Log(user_id=user_id, action=action, timestamp=datetime.utcnow())
     db.session.add(log_entry)
     db.session.commit()
-
     # Log to file and database
     logger.info(action, extra={'user_id': user_id})
