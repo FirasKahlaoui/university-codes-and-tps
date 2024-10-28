ff35d3e0de52f025b4fa157e0ef868f1b0f7138e
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-10-28 20:17:47 +0100

    js file

---
+++ MongoDB/TP2/employes/tp.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
